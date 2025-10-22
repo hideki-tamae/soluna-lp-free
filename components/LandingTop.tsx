c5aabe206dc7b0399e5132f2eff1690bcc5abf25
hideki-tamae <tamatixyan@gmail.com>
2025-10-22 23:07:09 +0900

    Update LandingTop.tsx

--- components/LandingTop.tsx
+++ components/LandingTop.tsx
@@ -89,7 +89,7 @@ export default function ランディングトップ() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                リバース・ラジオ（Spotify・準備中）
+                リバース・ラジオ（準備中）
               </a>
               <br />
               <a className="underline opacity-90 hover:opacity-100" href="#" target="_blank" rel="noopener noreferrer">
